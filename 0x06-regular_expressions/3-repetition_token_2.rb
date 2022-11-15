905c5da6a13c4a263b8f798fd621121799af73bd
Beloved-vince <vinceoludare@gmail.com>
2022-11-15 13:59:47 +0100

    regexp specific

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
-# This script check for range
+# This is a script that must match the pattern
+# "hbn,hbtn, hbttn, hbtttn, hbttttn" not "hbn"
 
-puts ARGV[0].scan(/hb+n/).join
+puts ARGV[0].scan(/hbt+n/).join
