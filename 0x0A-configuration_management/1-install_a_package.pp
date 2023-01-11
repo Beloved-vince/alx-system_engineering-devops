f4a0f9f1ec48202725b858fb588d0945b7f00398
Beloved-vince <vinceoludare@gmail.com>
2023-01-11 17:33:51 +0100

    fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install flask from pipe with the aid of Puppet
-package {'flask':
-  ensure => 'latest',
-  provider => 'pip',
+# Install flask from pip3 with the aid of Puppet
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
 }
 
